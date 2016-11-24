e4d628142508995de4c1dce36ff2afd6ccc5567c
Philip Zak <philipkzak@gmail.com>
2016-11-24 09:19:17 -0500

    forgot to add nav.php to commit

---
+++ nav.php
@@ -0,0 +1,15 @@
+<div class="row" id="header">
+  <div class="col-md-11 col-xs-11">
+    <h1>Places to Try</h1>
+  </div>
+  <div class="col-md-1 col-xs-1">
+    <button class="menu-btn">Menu</button>
+    <div class="menu">
+      <a class="close-btn">&times;</a>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="add.php">Add</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
